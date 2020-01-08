7495861c36bf98d1319d92ad5f7b896bc47a8e98
Emiko-Fukuhara <sano6de@gmail.com>
2020-01-08 06:45:32 +0000

    Change resource

--- config/routes.rb
+++ config/routes.rb
@@ -2,60 +2,34 @@ Rails.application.routes.draw do
   devise_for :customers
   devise_for :admins
   namespace :admin do
-    get 'order_items/update'
+    resources :order_items
   end
   namespace :admin do
-    get 'orders/index'
-    get 'orders/show'
-    get 'orders/update'
+    resources :orders
   end
   namespace :admin do
-    get 'customers/index'
-    get 'customers/show'
-    get 'customers/edit'
-    get 'customers/update'
+    resources :customers
   end
   namespace :admin do
-    get 'genres/index'
-    get 'genres/create'
-    get 'genres/edit'
-    get 'genres/update'
+    resources :genres
   end
   namespace :admin do
-    get 'products/index'
-    get 'products/new'
-    get 'products/create'
-    get 'products/show'
-    get 'products/edit'
-    get 'products/update'
+    resources :products
   end
   namespace :admin do
-    get 'homes/top'
+    root to: 'homes#top'
   end
-  get 'deliveries/index'
-  get 'deliveries/create'
-  get 'deliveries/destroy'
-  get 'deliveries/edit'
-  get 'deliveries/update'
-  get 'customers/show'
-  get 'customers/edit'
-  get 'customers/bye'
-  get 'customers/update'
-  get 'customers/update_dl'
-  get 'orders/create'
-  get 'orders/index'
-  get 'orders/new'
-  get 'orders/confirm'
-  get 'orders/show'
-  get 'orders/thanks'
-  get 'cart_items/create'
-  get 'cart_items/index'
-  get 'cart_items/update'
-  get 'cart_items/destroy'
-  get 'cart_items/destroy_all'
-  get 'products/index'
-  get 'products/show'
-  get 'homes/top'
+  resources :deliveries
+
+  resources :customers
+
+  resources :orders
+
+  resources :cart_items
+
+  resources :products
+
+  root to: 'homes#top'
   get 'homes/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
