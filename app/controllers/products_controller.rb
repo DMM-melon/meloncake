d15fcee5ebfb9a97acdf1610426e66efcb1bede4
KKeisuke0819 <koke0819@gmail.com>
2020-01-15 03:17:12 +0000

    [customer]

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   	@genres = Genre.where(genre_status: '0')
     @tax = 1.08
     @title = Genre.find_by(id: params[:genre_id]).variety
-    @products = Product.where(genre_id: params[:genre_id], product_status: '1')
+    @products = Product.where(genre_id: params[:genre_id], product_status: '0')
     # if文で構成したほうがいいとのこと
   	# genre_idがないproduct/のときは@products = Product.where(product_status: '1')
   end
