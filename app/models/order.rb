e68a2b6aa63e5ea726aa15a4a44a53c7c03ea633
Emiko-Fukuhara <sano6de@gmail.com>
2020-01-15 08:29:16 +0000

    0115 admin/order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,7 +4,7 @@ class Order < ApplicationRecord
 	enum order_status:{入金待ち: 0,入金確認: 1,製作中: 2,発送準備中: 3,発送済み: 4 }
 	# 自動更新設定すること
 
-	belongs_to :customer
+	belongs_to :customer, ->{with_deleted}
 	has_many :order_items
 
 	attr_accessor :address_option
