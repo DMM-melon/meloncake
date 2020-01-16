a96d2df621f3710386b00a06cbe66ddea4db46d9
Emiko-Fukuhara <sano6de@gmail.com>
2020-01-16 09:59:53 +0000

    admin/order,order_item customer/home,order

--- app/controllers/admin/order_items_controller.rb
+++ app/controllers/admin/order_items_controller.rb
@@ -1,4 +1,26 @@
 class Admin::OrderItemsController < ApplicationController
   def update
+  	@order_item = OrderItem.find(params[:id])
+    @order_item.update(order_item_params)
+
+    if @order_item.order_item_status == "製作中"
+          @order_item.order.order_status = "製作中"
+          @order_item.order.save
+    end
+
+    @order_items = @order_item.order.order_items
+    if @order_items.all?{|item| item.order_item_status == "製作完了" } #all?で全部該当かチェック
+      @order_item.order.order_status = "発送準備中"
+      @order_item.order.save
+    end
+
+    flash[:notice] = "● 製作ステータス更新しました"
+    redirect_to admin_order_path(@order_item.order)
   end
+
+    private
+    def order_item_params
+        params.require(:order_item).permit(:order_item_status)
+    end
+
 end
