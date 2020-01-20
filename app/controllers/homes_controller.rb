97b07f4d8e1e329498fbe5a0650ec546da85c33f
Emiko-Fukuhara <sano6de@gmail.com>
2020-01-20 06:01:22 +0000

    design

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,16 +1,16 @@
 class HomesController < ApplicationController
   def top
-  	@genres = Genre.where(genre_status: '0')
-  	@products = Product.joins(:genre).where("product_status = ? and genres.genre_status = ?", 0, 0).page(params[:page]).per(3)
-  	@tax = 1.08
+    @genres = Genre.where(genre_status: '0')
+    @products = Product.joins(:genre).where("product_status = ? and genres.genre_status = ?", 0, 0).page(params[:page]).per(9)
+    @tax = 1.08
   end
 
 
   def search
-  	@genres = Genre.where(genre_status: '0')
-  	@tax = 1.08
+    @genres = Genre.where(genre_status: '0')
+    @tax = 1.08
     #Viewのformで取得したパラメータをモデルに渡す
-    @products = Product.search(params[:search]).joins(:genre).where("product_status = ? and genres.genre_status = ?", 0, 0).page(params[:page]).per(3)
+    @products = Product.search(params[:search]).joins(:genre).where("product_status = ? and genres.genre_status = ?", 0, 0).page(params[:page]).per(9)
     render action: :top
   end
 
