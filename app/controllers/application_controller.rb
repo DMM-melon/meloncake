4215feecc6c9be925d932b7d2830d11ade732d5b
KKeisuke0819 <koke0819@gmail.com>
2020-01-20 06:51:05 +0000

    [test]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,6 @@ before_action :configure_permitted_parameters, if: :devise_controller?
       root_path
     end
   end
-
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :first_name_kana, :last_name_kana, :postcode, :address, :phone])
